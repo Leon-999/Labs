0b9209b1f632ee5c9594f074283caff5430cc120
Leon-999 <lionewr@gmail.com>
2013-05-21 03:04:19 +0400

    Begin new project - photo filters. Now working "open" , "save" with images and "exit" out of application.

---
+++ photoFilter/mainForm.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace photoFilter
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.canvas = new System.Windows.Forms.PictureBox();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.radioSourseFile = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).BeginInit();
+            this.menuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // canvas
+            // 
+            this.canvas.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.canvas.Location = new System.Drawing.Point(12, 27);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(535, 454);
+            this.canvas.TabIndex = 0;
+            this.canvas.TabStop = false;
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenu});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(739, 24);
+            this.menuStrip.TabIndex = 2;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // fileToolStripMenu
+            // 
+            this.fileToolStripMenu.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenu,
+            this.saveAsToolStripMenu,
+            this.exitToolStripMenu});
+            this.fileToolStripMenu.Name = "fileToolStripMenu";
+            this.fileToolStripMenu.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenu.Text = "File";
+            // 
+            // openToolStripMenu
+            // 
+            this.openToolStripMenu.Name = "openToolStripMenu";
+            this.openToolStripMenu.Size = new System.Drawing.Size(120, 22);
+            this.openToolStripMenu.Text = "Open";
+            this.openToolStripMenu.Click += new System.EventHandler(this.openToolStripMenu_Click);
+            // 
+            // saveAsToolStripMenu
+            // 
+            this.saveAsToolStripMenu.Name = "saveAsToolStripMenu";
+            this.saveAsToolStripMenu.Size = new System.Drawing.Size(120, 22);
+            this.saveAsToolStripMenu.Text = "Save as";
+            this.saveAsToolStripMenu.Click += new System.EventHandler(this.saveAsToolStripMenu_Click);
+            // 
+            // exitToolStripMenu
+            // 
+            this.exitToolStripMenu.Name = "exitToolStripMenu";
+            this.exitToolStripMenu.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenu.Text = "Exit";
+            this.exitToolStripMenu.Click += new System.EventHandler(this.exitToolStripMenu_Click);
+            // 
+            // radioSourseFile
+            // 
+            this.radioSourseFile.AutoSize = true;
+            this.radioSourseFile.Location = new System.Drawing.Point(606, 27);
+            this.radioSourseFile.Name = "radioSourseFile";
+            this.radioSourseFile.Size = new System.Drawing.Size(87, 17);
+            this.radioSourseFile.TabIndex = 3;
+            this.radioSourseFile.TabStop = true;
+            this.radioSourseFile.Text = "sourse image";
+            this.radioSourseFile.UseVisualStyleBackColor = true;
+            this.radioSourseFile.CheckedChanged += new System.EventHandler(this.radioSourseFile_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(580, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "What display?";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(606, 50);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(85, 17);
+            this.radioButton1.TabIndex = 5;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(739, 493);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioSourseFile);
+            this.Controls.Add(this.canvas);
+            this.Controls.Add(this.menuStrip);
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "mainForm";
+            this.Text = "MainForm";
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).EndInit();
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox canvas;
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenu;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenu;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenu;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenu;
+        private System.Windows.Forms.RadioButton radioSourseFile;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton1;
+    }
+}
+
---
+++ photoFilter/mainForm.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace photoFilter
+{
+    public partial class mainForm : Form
+    {
+
+        private OpenFileDialog openFileDialog;
+        private SaveFileDialog saveFileDialog;
+        private Bitmap sourceImage;
+        private Bitmap workImage;
+
+        private int canvasWidth, canvasHeight;
+
+        public mainForm()
+        {
+            InitializeComponent();
+
+            this.openFileDialog = new OpenFileDialog();
+            this.saveFileDialog = new SaveFileDialog();
+
+            this.canvasWidth = this.canvas.Width;
+            this.canvasHeight = this.canvas.Height;
+
+        }
+
+        private void openToolStripMenu_Click(object sender, EventArgs e)
+        {
+            this.openNewImage();
+        }
+
+        private void saveAsToolStripMenu_Click(object sender, EventArgs e)
+        {
+            saveNewImage();
+        }
+
+        private void exitToolStripMenu_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void openNewImage()
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                if (this.sourceImage != null) this.sourceImage.Dispose();
+                if (this.workImage != null)
+                {
+                    this.workImage.Dispose();
+                }
+
+                this.sourceImage = new Bitmap(this.openFileDialog.FileName);
+                this.workImage = new Bitmap(this.sourceImage.Width, this.sourceImage.Height);
+                //this.canvas.SizeMode = PictureBoxSizeMode.StretchImage;
+                if (this.sourceImage.Width > this.canvasWidth || sourceImage.Height > this.canvasHeight)
+                {
+                    double widthRatio, heightRatio;
+
+                    heightRatio = (double)this.canvasHeight / (double)sourceImage.Height;
+                    widthRatio = (double)this.canvasWidth / (double)sourceImage.Width;
+                    if (heightRatio < widthRatio)
+                    {
+                        canvas.Width = Convert.ToInt16(sourceImage.Width * heightRatio);
+                        canvas.Height = canvasHeight;
+                    }
+                    else
+                    {
+                        canvas.Width = canvasWidth;
+                        canvas.Height = Convert.ToInt16(sourceImage.Height * widthRatio);
+                    }
+                    this.canvas.SizeMode = PictureBoxSizeMode.StretchImage;
+                }
+                else
+                {
+                    //if (canvas.SizeMode == PictureBoxSizeMode.StretchImage) canvas.SizeMode = PictureBoxSizeMode.AutoSize;
+                    this.canvas.SizeMode = PictureBoxSizeMode.AutoSize;
+                }
+                this.workImage = this.sourceImage;
+                refreshCanvas();
+            }
+        }
+
+        private void refreshCanvas()
+        {
+            if (workImage != null)
+            {
+                canvas.Image = workImage;
+                canvas.Refresh();
+            }
+        }
+
+        private void saveNewImage()
+        {
+            if (workImage != null)
+            {
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    workImage.Save(saveFileDialog.FileName);
+                }
+            }
+        }
+
+        private void radioSourseFile_CheckedChanged(object sender, EventArgs e)
+        {
+            this.workImage = this.sourceImage;
+            this.refreshCanvas();
+        }
+
+
+        
+    }
+}
