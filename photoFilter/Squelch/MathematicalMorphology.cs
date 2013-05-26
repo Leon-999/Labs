e10bc862b6a011ef8792bbc99843f9ba218dfdac
Leon-999 <lionewr@gmail.com>
2013-05-26 17:44:31 +0400

    Added functions for convert image to binary matrix and inversely.

--- photoFilter/Squelch/MathematicalMorphology.cs
+++ photoFilter/Squelch/MathematicalMorphology.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Drawing;
 
 namespace photoFilter.Squelch
 {
@@ -63,11 +64,63 @@ namespace photoFilter.Squelch
             }
         }
 
+        public void setValue(int x, int y, bool value)
+        {
+            if ((x >= 0 && x < this.width) && (y >= 0 && y < this.height))
+                this.matrix[x][y] = value;
+        }
+
+        public bool getValue(int x, int y)
+        {
+            bool returned;
+
+            if ((x >= 0 && x < this.width) && (y >= 0 && y < this.height))
+                returned = this.matrix[x][y];
+            else
+                returned = false;
+
+            return returned;
+        }
+
 
     }
 
     abstract class MathematicalMorphology
     {
+        protected BinaryMatrix imageToBinaryMatrix(Bitmap sourceImage)
+        {
+            BinaryMatrix returned = new BinaryMatrix(sourceImage.Width, sourceImage.Height);
 
+            for (int i = 0; i < sourceImage.Height; ++i)
+                for (int j = 0; j < sourceImage.Width; ++j)
+                {
+                    Color currentPixel = sourceImage.GetPixel(i, j);
+                    if(currentPixel == Color.White) 
+                        returned.setValue(i, j, false);
+                    else
+                        returned.setValue(i, j, true);
+
+                }
+
+            return returned;
+        }
+
+        protected Bitmap binaryMatrixToImage(BinaryMatrix sourceMatrix)
+        {
+            Bitmap returned = new Bitmap(sourceMatrix.WIDTH, sourceMatrix.HEIGHT);
+
+            for (int i = 0; i < sourceMatrix.HEIGHT; ++i)
+                for (int j = 0; j < sourceMatrix.WIDTH; ++j)
+                {
+                    bool currentValue = sourceMatrix.getValue(i, j);
+                    if (currentValue == false)
+                        returned.SetPixel(i, j, Color.White);
+                    else
+                        returned.SetPixel(i, j, Color.Black);
+
+                }
+
+            return returned;
+        }
     }
 }
